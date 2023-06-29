d36d59ef58aa9d7f36751143cf0184d50049fddc
Fortunejite <fortunejite1234@gmail.com>
2023-06-29 09:05:52 +0100

    updates

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
-export function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    return { status: 200, body: 'Success' };
-  }).catch(() => {
-    return Error();
-  }).finally(() => {
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import uploadPhoto from 'utils.js'
-import createUser from 'utils.js'
+/* eslint-disable import/no-duplicates */
+import uploadPhoto from './utils';
+import createUser from './utils';
 
-export function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((values) => {
-            console.log(values[0].body, values[1].firstName, values[1].lastName);
-        })
-        .catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
